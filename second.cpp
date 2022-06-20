ba1a8174381049d819de22756da151cc0411fe5e
eyobtadele <eyobtadele2705@gmail.com>
2022-06-20 10:53:51 +0300

    loop

---
+++ second.cpp
@@ -0,0 +1,21 @@
+/ positive integers such as 1,2,3,...n are known as natural numbers
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num, sum;
+    sum = 0;
+
+    cout << "Enter a positive integer: ";
+    cin >> num;
+
+    for (int i = 1; i <= num; ++i) {
+        sum += i;
+    }
+
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
